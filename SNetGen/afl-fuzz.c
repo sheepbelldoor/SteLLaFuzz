3b0aee0f89b012e4df384ac192fc2928c0b00c4b
Jongmun Yang <sheepbelldoor.dev@gmail.com>
2025-04-08 13:58:48 +0900

    SNetGen is same as aflnet

--- SNetGen/afl-fuzz.c
+++ SNetGen/afl-fuzz.c
@@ -760,13 +760,12 @@ struct queue_entry *choose_seed(u32 target_state_id, u8 mode)
 }
 
 /* Update state-aware variables */
-int update_state_aware_variables(struct queue_entry *q, u8 dry_run)
+void update_state_aware_variables(struct queue_entry *q, u8 dry_run)
 {
   khint_t k;
   int discard, i;
   state_info_t *state;
   unsigned int state_count;
-  int is_interesting = 0;
 
   if (!response_buf_size || !response_bytes) return;
 
@@ -775,7 +774,6 @@ int update_state_aware_variables(struct queue_entry *q, u8 dry_run)
   q->unique_state_count = get_unique_state_count(state_sequence, state_count);
 
   if (is_state_sequence_interesting(state_sequence, state_count)) {
-    is_interesting = 1;
     //Save the current kl_messages to a file which can be used to replay the newly discovered paths on the ipsm
     u8 *temp_str = state_sequence_to_string(state_sequence, state_count);
     u8 *fname = alloc_printf("%s/replayable-new-ipsm-paths/id:%llu:%s:%s", out_dir, get_cur_time() / 1000, temp_str, dry_run ? basename(q->fname) : "new");
@@ -985,8 +983,6 @@ int update_state_aware_variables(struct queue_entry *q, u8 dry_run)
 
   //Free state sequence
   if (state_sequence) ck_free(state_sequence);
-
-  return is_interesting;
 }
 
 /* Send (mutated) messages in order to the server under test */
@@ -1658,93 +1654,6 @@ static void add_to_queue(u8* fname, u32 len, u8 passed_det) {
   }
 }
 
-static void remove_from_queue(struct queue_entry* q) {
-    // Validity check
-    if (!q || !queue) return;
-
-    // Find q's position in the queue - track the previous element
-    struct queue_entry *prev = NULL;
-    struct queue_entry *current = queue;
-    
-    while (current && current != q) {
-        prev = current;
-        current = current->next;
-    }
-    
-    // Exit if q is not in the queue
-    if (!current) return;
-    
-    // Remove element from queue
-    if (!prev) {
-        // If q is the first element
-        queue = q->next;
-    } else {
-        // If q is in the middle or at the end
-        prev->next = q->next;
-    }
-    
-    // Update queue_top
-    if (q == queue_top) {
-        queue_top = prev; // Previous element becomes the new top
-    }
-    
-    // Update queue_cur - move to next element if current one is being removed
-    if (q == queue_cur) {
-        queue_cur = q->next;
-    }
-    
-    // Reconstruct next_100 relationships
-    if (queue) {
-        struct queue_entry *p = queue;
-        q_prev100 = p;
-        
-        u32 count = 0;
-        while (p) {
-            count++;
-            if (count % 100 == 0 && p->next) {
-                q_prev100->next_100 = p->next;
-                q_prev100 = p->next;
-            }
-            p = p->next;
-        }
-        
-        // Handle the last segment
-        if (q_prev100 && q_prev100 != p) {
-            q_prev100->next_100 = NULL;
-        }
-    } else {
-        // If queue becomes empty
-        q_prev100 = NULL;
-    }
-    
-    // Update top_rated array (if this element is referenced)
-    for (u32 i = 0; i < MAP_SIZE; i++) {
-        if (top_rated[i] == q) {
-            top_rated[i] = NULL; // Remove reference
-        }
-    }
-    
-    // Update counters
-    queued_paths--;
-    
-    // Decrease pending_not_fuzzed if q hasn't been fuzzed yet
-    if (!q->was_fuzzed) {
-        pending_not_fuzzed--;
-    }
-    
-    // Free memory
-    ck_free(q->fname);
-    if (q->trace_mini) ck_free(q->trace_mini);
-    
-    // Free AFLNet-specific data structures
-    for (u32 i = 0; i < q->region_count; i++) {
-        if (q->regions[i].state_sequence) ck_free(q->regions[i].state_sequence);
-    }
-    if (q->regions) ck_free(q->regions);
-    
-    // Finally free the queue_entry itself
-    ck_free(q);
-}
 
 /* Destroy the entire queue. */
 
@@ -3677,9 +3586,8 @@ static void perform_dry_run(char** argv) {
     res = calibrate_case(argv, q, use_mem, 0, 1);
     ck_free(use_mem);
 
-    int is_interesting = -1;
     /* Update state-aware variables (e.g., state machine, regions and their annotations */
-    if (state_aware_mode) is_interesting = update_state_aware_variables(q, 1);
+    if (state_aware_mode) update_state_aware_variables(q, 1);
 
     /* save the seed to file for replaying */
     u8 *fn_replay = alloc_printf("%s/replayable-queue/%s", out_dir, basename(q->fname));
@@ -3838,18 +3746,7 @@ static void perform_dry_run(char** argv) {
 
     if (q->var_behavior) WARNF("Instrumentation output varies across runs.");
 
-    if (is_interesting == 1) {
-      SAYF(cLGN "[+] " cRST "Test case '%s' has interesting state sequence\n", fn);
-      q = q->next;
-    } else if (is_interesting == 0) {
-      SAYF(cLRD "[-] " cRST "Test case '%s' has no interesting state, remove it from the queue\n", fn);
-      struct queue_entry* remove_q = q;
-      q = q->next;
-      remove_from_queue(remove_q);
-    } else {
-      SAYF(cLRD "[T.T] " cRST "This statement should not happen. in '%s'\n", fn);
-      q = q->next;
-    }
+    q = q->next;
 
   }
 
